4b1c6a8460639768146693930415a87f0a194a52
dev <dev@local.local>
2024-12-16 15:34:33 +0700

    terraform fmt

--- main.tf
+++ main.tf
@@ -8,9 +8,9 @@ module "ecs" {
 
 module "flask-echo" {
   source = "./modules/flask-echo"
-  
-  cluster_id           = module.ecs.cluster_id
-  vpc_id              = module.ecs.vpc_id
-  private_subnet_ids  = module.ecs.private_subnet_ids
-  container_image     = "thatthep/flask-echo:main"
+
+  cluster_id         = module.ecs.cluster_id
+  vpc_id             = module.ecs.vpc_id
+  private_subnet_ids = module.ecs.private_subnet_ids
+  container_image    = "thatthep/flask-echo:main"
 } 
